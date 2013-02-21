0bc169acbccb6e336a1f1158157f27665fad26d8
Ross Karchner <ross.karchner@cfpb.gov>
2013-02-21 09:49:59 -0500

    PEP8 cleanup on models.py

--- comparisontool/models.py
+++ comparisontool/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-# Create your models here.
 
 class School(models.Model):
     """Represents a school"""
@@ -8,30 +7,29 @@ class School(models.Model):
     data_json = models.TextField()
 
     def __unicode__(self):
-        return  self.primary_alias + u"(%s)" % self.school_id
+        return self.primary_alias + u"(%s)" % self.school_id
 
     @property
     def primary_alias(self):
         return self.alias_set.get(is_primary=True).alias
-    
 
 
 class Alias(models.Model):
-    institution=models.ForeignKey(School)
-    alias=models.TextField()
-    is_primary=models.BooleanField(default=False)
-    
+    institution = models.ForeignKey(School)
+    alias = models.TextField()
+    is_primary = models.BooleanField(default=False)
+
     def __unicode__(self):
-        return u"%s (alias for %s)"%(self.alias, unicode(self.institution))
-        
-        
+        return u"%s (alias for %s)" % (self.alias, unicode(self.institution))
+
+
 class Worksheet(models.Model):
-    guid=models.CharField(max_length=64, primary_key=True)
-    saved_data=models.TextField()
-    created=models.DateTimeField(auto_now_add=True)
-    updated=models.DateTimeField(auto_now=True)
+    guid = models.CharField(max_length=64, primary_key=True)
+    saved_data = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
 
 
 class Feedback(models.Model):
-    created=models.DateTimeField(auto_now_add=True)
-    message=models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    message = models.TextField()
