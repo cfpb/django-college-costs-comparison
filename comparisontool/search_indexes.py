538ea0c1f30ff5a2f5da083f54c137127bfc2570
Andrew T. Baker <andrew.tork.baker@gmail.com>
2013-12-06 15:59:23 -0500

    Updating search_indexes for haystack 2.1

--- comparisontool/search_indexes.py
+++ comparisontool/search_indexes.py
@@ -1,21 +1,21 @@
-from haystack.indexes import *
-from haystack import site
+from haystack import indexes
 from comparisontool.models import School
 
 
-class SchoolIndex(SearchIndex):
-    text = CharField(document=True, model_attr='primary_alias')
-    autocomplete = EdgeNgramField()
-    school_id = IntegerField(model_attr='school_id')
-    city = CharField(model_attr='city')
-    state = CharField(model_attr='state')
+class SchoolIndex(indexes.SearchIndex):
+    text = indexes.CharField(document=True, model_attr='primary_alias')
+    autocomplete = indexes.EdgeNgramField()
+    school_id = indexes.IntegerField(model_attr='school_id')
+    city = indexes.CharField(model_attr='city')
+    state = indexes.CharField(model_attr='state')
 
-    def index_queryset(self):
+    def get_model(self):
+        return School
+
+    def index_queryset(self, using=None):
         """Used when the entire index for model is updated."""
-        return School.objects.all()
+        return self.get_model().objects.all()
 
     def prepare_autocomplete(self, obj):
         alias_strings = [a.alias for a in obj.alias_set.all()]
         return ' '.join(alias_strings)
-
-site.register(School, SchoolIndex)
