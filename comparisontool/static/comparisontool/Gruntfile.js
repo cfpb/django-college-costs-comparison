430cab711aff8673c6002fd819e4edfc720aba04
davisd <daniel.davis@excella.com>
2014-02-11 16:53:17 -0500

    Made some changes to the grunt file to cleanup some duplicate code and consolidate it.

--- comparisontool/static/comparisontool/Gruntfile.js
+++ comparisontool/static/comparisontool/Gruntfile.js
@@ -4,24 +4,29 @@ module.exports = function(grunt) {
     if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
   }
 
-  var browsers = [{
-      browserName: "firefox",
-      version: "19",
-      platform: "XP"
+  var browsers = [
+  {
+    browserName: 'firefox',
+    version: '20',
+    platform: 'XP'
   }, {
-      browserName: "chrome",
-      platform: "XP"
+    browserName: 'chrome',
+    platform: 'XP'
   }, {
-      browserName: "chrome",
-      platform: "linux"
+    browserName: 'chrome',
+    platform: 'linux'
   }, {
-      browserName: "internet explorer",
-      platform: "WIN8",
-      version: "10"
+    browserName: 'internet explorer',
+    platform: 'WIN8',
+    version: '10'
   }, {
-      browserName: "internet explorer",
-      platform: "VISTA",
-      version: "9"
+    browserName: 'internet explorer',
+    platform: 'WIN7',
+    version: '9'
+  }, {
+    browserName: 'internet explorer',
+    platform: 'VISTA',
+    version: '9'
   }];
 
   grunt.initConfig({
@@ -35,63 +40,33 @@ module.exports = function(grunt) {
     },
     watch: {},
     jasmine: {
-      test: {
-        src: ['js/*.js', '!js/data.js'],
-        options: {
-          specs: 'test/spec/ct-spec.js',
-          keepRunner: true,
-          // order matters here, jquery.js must be loaded before jasmine-jquery!
-          vendor: ['cfpb-files/jquery-1.5.1.min.js', 'cfpb-files/jasmine-jquery.js', 'cfpb-files/jasmine-jsreporter.js'],
-          junit: {
-            path: 'test/junit-results',
-            consolidate: true
-          },
-          template: require('grunt-template-jasmine-istanbul'),
-          templateOptions: {
-            coverage: 'test/coverage/coverage.json',
-            report: [
-              {
-                type: 'cobertura',
-                options: { 
-                  dir: 'test/coverage/report/cobertura'
-                }
-              },
-              {
-                type: 'html',
-                options: {
-                  dir: 'test/coverage/report/html'
-                }
+      src: ['js/*.js', '!js/data.js'],
+      options: {
+        specs: 'test/spec/ct-spec.js',
+        keepRunner: true,
+        // order matters here, jquery.js must be loaded before jasmine-jquery!
+        vendor: ['cfpb-files/jquery-1.5.1.min.js', 'cfpb-files/jasmine-jquery.js', 'cfpb-files/jasmine-jsreporter.js'],
+        junit: {
+          path: 'test/junit-results',
+          consolidate: true
+        },
+        template: require('grunt-template-jasmine-istanbul'),
+        templateOptions: {
+          coverage: 'test/coverage/coverage.json',
+          report: [
+            {
+              type: 'cobertura',
+              options: { 
+                dir: 'test/coverage/report/cobertura'
               }
-            ]
-          }
-        }
-      },
-      sauce: {
-        src: ['js/*.js', '!js/data.js'],
-        options: {
-          specs: 'test/spec/ct-spec.js',
-          timeout: 30000,
-          keepRunner: true,
-          // order matters here, jquery.js must be loaded before jasmine-jquery!
-          vendor: ['cfpb-files/jquery-1.5.1.min.js', 'cfpb-files/jasmine-jquery.js', 'cfpb-files/jasmine-jsreporter.js'],
-          template: require('grunt-template-jasmine-istanbul'),
-          templateOptions: {
-            coverage: 'test/coverage/coverage.json',
-            report: [
-              {
-                type: 'cobertura',
-                options: { 
-                  dir: 'test/coverage/report/cobertura'
-                }
-              },
-              {
-                type: 'html',
-                options: {
-                  dir: 'test/coverage/report/html'
-                }
+            },
+            {
+              type: 'html',
+              options: {
+                dir: 'test/coverage/report/html'
               }
-            ]
-          }
+            }
+          ]
         }
       }
     },
@@ -102,44 +77,20 @@ module.exports = function(grunt) {
           //username: This value is set in the environment variable SAUCE_USERNAME
           //key: This value is set in the environment variable SAUCE_ACCESS_KEY
           detailedError: true,
-          testname: 'Paying-for-College',
+          testname: 'Paying-for-College JS Unit Tests',
           testTimeout: 180000,
           tags: [
             'cf.gov',
             'paying-for-college'
           ],
-          browsers: [
-            {
-                browserName: 'firefox',
-                version: '20',
-                platform: 'XP'
-            }, {
-                browserName: 'chrome',
-                platform: 'XP'
-            }, {
-                browserName: 'chrome',
-                platform: 'linux'
-            }, {
-                browserName: 'internet explorer',
-                platform: 'WIN8',
-                version: '10'
-            }, {
-                browserName: 'internet explorer',
-                platform: 'WIN7',
-                version: '9'
-            }, {
-                browserName: 'internet explorer',
-                platform: 'VISTA',
-                version: '9'
-            }
-          ]
+          browsers: browsers
         }
       }
     }
   });
 
   grunt.registerTask("dev", ["connect", "watch"]);
-  grunt.registerTask("test", ["jasmine:test"]);
-  grunt.registerTask("sauce", ["connect", "jasmine:sauce", "saucelabs-jasmine"]);
+  grunt.registerTask("test", ["jasmine"]);
+  grunt.registerTask("sauce", ["connect", "jasmine", "saucelabs-jasmine"]);
 
 };
