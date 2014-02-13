664e63cc5a53272753e62158605ebf8487145cbf
davisd <daniel.davis@excella.com>
2014-02-13 12:39:18 -0500

    Another experiment with the parameters for Jenkins

--- comparisontool/static/comparisontool/Gruntfile.js
+++ comparisontool/static/comparisontool/Gruntfile.js
@@ -6,9 +6,7 @@ module.exports = function(grunt) {
 
   console.log("Build ID: ");
   console.log(process.env.BUILD_ID);
-  sauce_identifier = Math.floor((new Date()).getTime() / 1000 - 1230768000).toString();
-  console.log("Sauce identifier: " + sauce_identifier);
-
+  
   var browsers = [
   {
     browserName: 'firefox',
@@ -84,7 +82,6 @@ module.exports = function(grunt) {
           //username: This value is set in the environment variable SAUCE_USERNAME
           //key: This value is set in the environment variable SAUCE_ACCESS_KEY
           detailedError: true,
-          identifier: sauce_identifier,
           testname: 'Paying-for-College JS Unit Tests',
           testTimeout: 180000,
           build: process.env.BUILD_ID, // Set by Jenkins
@@ -99,7 +96,7 @@ module.exports = function(grunt) {
   });
 
   // This line is required so that Jenkins can pick up the results for display
-  console.log("SauceOnDemandSessionID="+sauce_identifier+" job-name=Paying-for-College JS Unit Tests");
+  console.log("SauceOnDemandSessionID="+process.env.BUILD_ID+" job-name=Paying-for-College JS Unit Tests");
   grunt.registerTask("dev", ["connect", "watch"]);
   grunt.registerTask("test", ["jasmine"]);
   grunt.registerTask("sauce", ["connect", "jasmine", "saucelabs-jasmine"]);
