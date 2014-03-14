8780f0fa475b06dd02bbb62ca441508fef25de40
davidakennedy <david.kennedy@cfpb.gov>
2014-03-14 12:02:44 -0400

    Fix API URLs. H/T Andrew Baker.

--- comparisontool/urls.py
+++ comparisontool/urls.py
@@ -10,28 +10,28 @@ urlpatterns = patterns(
     url(r'^repay-student-debt/$', TemplateView.as_view(template_name='comparisontool/repay_student_debt.html'), name='pfc-repay'),
 
     url(r'^compare-financial-aid-and-college-cost/$', BuildComparisonView.as_view(), name='worksheet'),
-    url(r'^api/email/$', EmailLink.as_view(), name='email'),
+    url(r'^compare-financial-aid-and-college-cost/api/email/$', EmailLink.as_view(), name='email'),
 
-    url(r'^feedback/$',
+    url(r'^compare-financial-aid-and-college-cost/feedback/$',
         FeedbackView.as_view(),
         name='pfc-feedback'),
 
-    url(r'^technote/$',
+    url(r'^compare-financial-aid-and-college-cost/technote/$',
         TemplateView.as_view(template_name="comparisontool/technote.html"),
         name='pfc-technote'),
 
-    url(r'^api/search-schools.json', school_search_api),
-    url(r'^api/bah-lookup.json', bah_lookup_api),
+    url(r'^compare-financial-aid-and-college-cost/api/search-schools.json', school_search_api),
+    url(r'^compare-financial-aid-and-college-cost/api/bah-lookup.json', bah_lookup_api),
 
-    url(r'^api/school/(\d+).json',
+    url(r'^compare-financial-aid-and-college-cost/api/school/(\d+).json',
         SchoolRepresentation.as_view(),
         name='school-json'),
 
-    url(r'^api/worksheet/([1-z0-9-]*).json$',
+    url(r'^compare-financial-aid-and-college-cost/api/worksheet/([1-z0-9-]*).json$',
         DataStorageView.as_view(),
         name='worksheet'),
 
-    url(r'^api/worksheet/$',
+    url(r'^compare-financial-aid-and-college-cost/api/worksheet/$',
         CreateWorksheetView.as_view(),
         name='create_worksheet')
 )
