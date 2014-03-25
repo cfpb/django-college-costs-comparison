312c9f62d8e70c4540e02f41785de5e05525811c
davidakennedy <david.kennedy@cfpb.gov>
2014-03-25 12:20:29 -0400

    Open cost of attendance area on continue when adding a school per UX.

--- comparisontool/static/comparisontool/js/comparison-tool.js
+++ comparisontool/static/comparisontool/js/comparison-tool.js
@@ -1532,6 +1532,7 @@ var CFPBComparisonTool = (function() {
             $("#step-three .continue").click( function() {
             	clearAddForms();
                 setAddStage(0);
+                $("#open").trigger("click");
                 $(document.body).animate({'scrollTop':   $("#comparison-tables").offset().top }, 750);
             });
 
