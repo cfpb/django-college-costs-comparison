24d5f86ed94f626afc423bf8de9b3d58d288612f
davidakennedy <david.kennedy@cfpb.gov>
2014-05-06 19:24:25 -0400

    First pass at modal overlay for save feature.

--- comparisontool/static/comparisontool/js/comparison-tool.js
+++ comparisontool/static/comparisontool/js/comparison-tool.js
@@ -1495,7 +1495,172 @@ var CFPBComparisonTool = (function() {
 
         } // end .updateFormValues()
 
-    } // end Column() class   
+    } // end Column() class
+
+    //== Functiond for Save modal dialog ==//
+    //== Code from https://github.com/gdkraus/accessible-modal-dialog ==//
+
+    /*
+ 
+     ============================================
+     License for Application
+     ============================================
+     
+     This license is governed by United States copyright law, and with respect to matters
+     of tort, contract, and other causes of action it is governed by North Carolina law,
+     without regard to North Carolina choice of law provisions.  The forum for any dispute
+     resolution shall be in Wake County, North Carolina.
+     
+     Redistribution and use in source and binary forms, with or without modification, are
+     permitted provided that the following conditions are met:
+     
+     1. Redistributions of source code must retain the above copyright notice, this list
+     of conditions and the following disclaimer.
+     
+     2. Redistributions in binary form must reproduce the above copyright notice, this
+     list of conditions and the following disclaimer in the documentation and/or other
+     materials provided with the distribution.
+     
+     3. The name of the author may not be used to endorse or promote products derived from
+     this software without specific prior written permission.
+     
+     THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR IMPLIED
+     WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+     AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE
+     LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+     DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+     LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+     THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+     NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+     ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+     
+     */
+
+     // jQuery formatted selector to search for focusable items when modal launches
+    var focusableElementsString = "a[href], area[href], input:not([disabled]), select:not([disabled]), textarea:not([disabled]), button:not([disabled]), iframe, object, embed, *[tabindex], *[contenteditable]";
+
+    // Store the item that has focus before opening the modal window
+    var focusedElementBeforeModal;
+
+    function trapSpaceKey(obj, evt, f) {
+        // If space key pressed
+        if (evt.which == 32) {
+            // Fire the user passed event
+            f();
+            evt.preventDefault();
+        }
+    }
+
+    function trapEscapeKey(obj, evt) {
+
+        // If escape pressed
+        if (evt.which == 27) {
+
+            // Get list of all children elements in given object
+            var o = obj.find('*');
+
+            // Get list of focusable items
+            var cancelElement;
+            cancelElement = o.filter("#close-modal")
+
+            // Close the modal window
+            cancelElement.click();
+            evt.preventDefault();
+        }
+
+    }
+
+    function trapTabKey(obj, evt) {
+
+        // If tab or shift-tab pressed
+        if (evt.which == 9) {
+
+            // Get list of all children elements in given object
+            var o = obj.find('*');
+
+            // Get list of focusable items
+            var focusableItems;
+            focusableItems = o.filter(focusableElementsString).filter(':visible')
+
+            // Get currently focused item
+            var focusedItem;
+            focusedItem = jQuery(':focus');
+
+            // Get the number of focusable items
+            var numberOfFocusableItems;
+            numberOfFocusableItems = focusableItems.length
+
+            // get the index of the currently focused item
+            var focusedItemIndex;
+            focusedItemIndex = focusableItems.index(focusedItem);
+
+            if (evt.shiftKey) {
+                // Back tab
+                // If focused on first item and user preses back-tab, go to the last focusable item
+                if (focusedItemIndex == 0) {
+                    focusableItems.get(numberOfFocusableItems - 1).focus();
+                    evt.preventDefault();
+                }
+
+            } else {
+                // Forward tab
+                // If focused on the last item and user preses tab, go to the first focusable item
+                if (focusedItemIndex == numberOfFocusableItems - 1) {
+                    focusableItems.get(0).focus();
+                    evt.preventDefault();
+                }
+            }
+        }
+
+    }
+
+    function setInitialFocusModal(obj) {
+        // Get list of all children elements in given object
+        var o = obj.find("*");
+
+        // Set focus to first focusable item
+        var focusableItems;
+        focusableItems = o.filter(focusableElementsString).filter(":visible").first().focus();
+
+    }
+
+    function enterButtonModal() {
+        // BEGIN logic for executing the Enter button action for the modal window
+        alert("form submitted");
+        // END logic for executing the Enter button action for the modal window
+        hideModal();
+    }
+
+    function showModal(obj) {
+        $("#page").attr("aria-hidden", "true"); // mark the main page as hidden
+        $("#modal-overlay").css("display", "block"); // insert an overlay to prevent clicking and make a visual change to indicate the main apge is not available
+        $("#modal").css("display", "block"); // make the modal window visible
+        $("#modal").attr("aria-hidden", "false"); // mark the modal window as visible
+
+        // Save current focus
+        focusedElementBeforeModal = $(":focus");
+
+        // Get list of all children elements in given object
+        var o = obj.find('*');
+
+        // Safari and VoiceOver shim
+        // If VoiceOver in Safari is used, set the initial focus to the modal window itself instead of the first keyboard focusable item. This causes VoiceOver to announce the aria-labelled attributes. Otherwise, Safari and VoiceOver will not announce the labels attached to the modal window.
+
+        // Set the focus to the first keyboard focusable item
+        o.filter(focusableElementsString).filter(":visible").first().focus();
+
+
+    }
+
+    function hideModal() {
+        $("#modal-overlay").css("display", "none"); // remove the overlay in order to make the main screen available again
+        $("#modal").css("display", "none"); // hide the modal window
+        $("#modal").attr("aria-hidden", "true"); // mark the modal window as hidden
+        $("#page").attr("aria-hidden", "false"); // mark the main page as visible
+
+        // set focus back to element that had it before the modal was opened
+        focusedElementBeforeModal.focus();
+    }
 
     //-----------------------//
     //    DOCUMENT.READY     //
@@ -1569,6 +1734,32 @@ var CFPBComparisonTool = (function() {
         //---------------------------//
         //    JQUERY EVENT HANDLERS
         //---------------------------//
+
+            // Modal save dialog
+            $( "body" ).append( "<div id='modal-overlay' tabindex='-1'></div>" );
+
+            $("#save-and-share").click(function(e) {
+                showModal($('#modal'));
+            });
+            $("#enter").click(function(e) {
+                enterButtonModal();
+            });
+            $("#close-modal").click(function(e) {
+                hideModal();
+            });
+            $("#close-modal").keydown(function(event) {
+                trapSpaceKey($(this), event, hideModal);
+            });
+            $("html").on("click", "#modal-overlay", function(e) {
+                hideModal();
+                $("html").off("click");
+            });
+            $("#modal").keydown(function(event) {
+                trapTabKey($(this), event);
+            });
+            $("#modal").keydown(function(event) {
+                trapEscapeKey($(this), event);
+            });
 
             // Accordions (not the instrument, sadly)
             $('tr.show').click(function() {
@@ -2078,7 +2269,6 @@ var CFPBComparisonTool = (function() {
                             + "#"
                             + global.worksheet_id;
                 $("#unique").val(geturl);
-                $("#save-drawer").slideDown(300);
                 var t  = new Date();
                 var minutes = t.getMinutes();
                 if ( minutes < 10 ) {
