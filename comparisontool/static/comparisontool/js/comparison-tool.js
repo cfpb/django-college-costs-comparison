24e323e29da9139e804f1eca1378f5b6025f31fc
davidakennedy <david.kennedy@cfpb.gov>
2014-05-09 19:22:55 -0400

    Fix focus indicator on accordion and HTML errors on hidden elements for visuals.

--- comparisontool/static/comparisontool/js/comparison-tool.js
+++ comparisontool/static/comparisontool/js/comparison-tool.js
@@ -1742,7 +1742,8 @@ var CFPBComparisonTool = (function() {
             // Set up special vertical tabbing for comparison-tables
             for (c = 1; c <= 3; c++) {
                 var tabOrder = 1;
-                $('[data-column="' + c + '"]').find("input.school-data").each(function() {
+                var selectors = "input.school-data, .gibill-calculator, .rate-change, .arrw-click";
+                $('[data-column="' + c + '"]').find(selectors).each(function() {
                     var i = (c * 100) + tabOrder;
                     $(this).attr("data-tab-order", i);
                     tabOrder++;
