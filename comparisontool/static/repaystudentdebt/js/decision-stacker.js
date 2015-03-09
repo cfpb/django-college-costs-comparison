f00b2f4e26d52022553010c5d7f1920b9050f030
wernerc <charles.werner@cfpb.gov>
2015-03-09 14:51:19 -0400

    First commit of new Repaying Student Debt

---
+++ comparisontool/static/repaystudentdebt/js/decision-stacker.js
@@ -0,0 +1,78 @@
+/*
+ * Decision tree where each previous choice is visible
+ */
+(function ($) {
+
+  function assignButtons( code ) {
+    // this function relies on decisionStackerTargets object
+    if (typeof decisionStackerTargets !== "object") {
+      return false;
+    }
+    $elem = $('#q' + decisionStackerTargets[code]['question']);
+    var sectionOrigin = $elem.attr('data-ds-origin');
+    var sectionData = decisionStackerTargets[sectionOrigin];
+    $elem.find('button').each(function() {
+      var name = $(this).attr('data-name');
+      if ( sectionData.hasOwnProperty( name ) ) {
+        $(this).val(sectionData['question'] + sectionData[name]);
+      }
+    });
+  }
+
+  $.fn.decisionStacker = function () {
+    assignButtons( '0' );
+    // Set buttons up to lead to the next section, show appropriate text.
+    $('.ds-section .ds-buttons button').click( function() {
+      var code = $(this).val();
+
+      var destination = decisionStackerTargets[code]['question'];
+      // destination is a question
+      if ( destination !== undefined ) {
+        destination = '#q' + destination;
+      }
+      // destination is a module
+      else {
+        destination = '#' + decisionStackerTargets[code]['module'];
+      }
+      $( destination ).slideDown( 200, function() {
+        $( 'html, body' ).animate({
+            scrollTop: $( destination ).offset().top
+        }, 800);
+      });
+      $( destination ).attr('data-ds-origin', code);
+      assignButtons( code );
+      var $section = $(this).closest('.ds-section');
+      $section.find('[data-responds-to="' + $(this).attr('data-name') + '"]').show();
+      $section.find('.ds-content').slideUp( 200 );
+      $('.ds-clear-all').show();
+
+    });
+    $('.ds-response-container .go-back').click( function() {
+      var $section = $(this).closest('.ds-section');
+      var questionNumber = Number($section.attr('data-ds-qnum'));
+      $('.ds-question').each( function() {
+        if ( Number($(this).attr('data-ds-qnum')) > questionNumber ) {
+          $(this).find('.ds-content').show();
+          $(this).find('.ds-response-container div').hide();
+          $(this).hide();
+        }
+      });
+      $section.find( '.ds-response-container div' ).hide();
+      $('.ds-module').hide();
+      $section.find( '.ds-content' ).slideDown(200, function(){
+        $( 'html, body' ).animate({
+            scrollTop: $section.offset().top
+        }, 800);
+      });
+      if ( questionNumber === 1 ) {
+        $('.ds-clear-all').hide();
+      }
+    });
+    $('.ds-clear-button').click( function() {
+      $('#q1 .go-back').click();
+    });
+  };
+
+}(jQuery));
+
+
