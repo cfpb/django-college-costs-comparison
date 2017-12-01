3042d9ee95054084765d4193168b2ef3c70c610f
Ross Karchner <ross.karchner@cfpb.gov>
2017-12-01 13:13:06 -0500

    Improve security and robustness, remove some useless code

--- comparisontool/forms.py
+++ comparisontool/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from localflavor.us.forms import USZipCodeField
+
 from .validators import validate_uuid4
 
 
@@ -10,3 +12,11 @@ class FeedbackForm(forms.Form):
 class EmailForm(forms.Form):
     id = forms.CharField(validators=[validate_uuid4])
     email = forms.EmailField()
+
+
+class SchoolSearchForm(forms.Form):
+    q = forms.CharField(max_length=100)
+
+
+class BAHZipSearchForm(forms.Form):
+    zip5 = USZipCodeField()
