d229395774ceefd9cceeaeb88c38609d6258b6b1
Andy Chosak <andy@chosak.org>
2018-04-23 17:05:31 -0400

    rename tests to be more descriptive.
    thx @richaagarwal

--- comparisontool/tests/management/commands/test_load_bah.py
+++ comparisontool/tests/management/commands/test_load_bah.py
@@ -7,7 +7,7 @@ from comparisontool.models import BAHRate
 
 
 class LoadBAHTests(TestCase):
-    def test_load_csv(self):
+    def test_load_from_csv_creates_bahrate_objects(self):
         with tempfile.NamedTemporaryFile() as tf:
             tf.write('ZIP,BAH\n22203,123\n22205,0\n22207,456\n')
             tf.seek(0)
