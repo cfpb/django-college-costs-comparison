5b0b5977dc0fb627e9776c60a7571fa8d206c6df
Betsy Lorton <elorton@gmail.com>
2018-05-16 14:13:23 -0400

    Update custom management commands for Django>=1.10
    commands: load_bah and load_school
    
    They can still be called from django-admin in the same way:
    ./manage.py load_school file-of-schools.txt

--- comparisontool/management/commands/load_bah.py
+++ comparisontool/management/commands/load_bah.py
@@ -1,3 +1,5 @@
+from __future__ import print_function
+
 from glob import glob
 from json import dumps
 import csv
@@ -8,16 +10,18 @@ from comparisontool.models import *
 
 
 class Command(BaseCommand):
-    args = '<path>'
     help = 'Load the specified CSV into the BAH database'
 
+    def add_arguments(self, parser):
+        parser.add_argument('path_spec')
+
     def handle(self, *args, **options):
-        for path_spec in args:
-            for path in glob(path_spec):
-                print "loading %s" % path
-                with open(path) as csv_file:
-                    reader = csv.DictReader(csv_file)
-                    BAHRate.objects.all().delete()
-                    for record in reader:
-                        new_rate = BAHRate(zip5=record['ZIP'], value=record['BAH'])
-                        new_rate.save()
+        path_spec = options['path_spec']
+        for path in glob(path_spec):
+            print("loading BAH file:", path)
+            with open(path) as csv_file:
+                reader = csv.DictReader(csv_file)
+                BAHRate.objects.all().delete()
+                for record in reader:
+                    new_rate = BAHRate(zip5=record['ZIP'], value=record['BAH'])
+                    new_rate.save()
